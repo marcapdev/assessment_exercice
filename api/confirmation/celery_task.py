693f484dc983fd2c77d83c2d39abccd81a3de667
Marc <marc.amposta.perez93@gmail.com>
2022-10-19 17:27:15 +0200

    added celery task decorator shared_task

--- api/confirmation/celery_task.py
+++ api/confirmation/celery_task.py
@@ -2,21 +2,25 @@ from celery import shared_task
 import time
 
 
+@shared_task(autoretry_for=(Exception,), retry_kwargs={'max_retries': 2, 'countdown': 2})
 def send_sms():
     time.sleep(10)
     return "Success SMS"
 
 
+@shared_task(autoretry_for=(Exception,), retry_kwargs={'max_retries': 2, 'countdown': 2})
 def send_mail():
     time.sleep(10)
     return "Success MAIL"
 
 
+@shared_task(autoretry_for=(Exception,), retry_kwargs={'max_retries': 2, 'countdown': 2})
 def send_sms_mockup():
     time.sleep(20)
     return "Success MOCKUP SMS"
 
 
+@shared_task(autoretry_for=(Exception,), retry_kwargs={'max_retries': 2, 'countdown': 2})
 def send_mail_mockup():
     time.sleep(20)
     return "Success MOCKUP MAIL"
