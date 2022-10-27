e86c5d4a9a35ddeaf65fbf62d307bd507d47ae6a
Marc <marc.amposta.perez93@gmail.com>
2022-10-27 17:11:43 +0200

    fix

--- api/message/celery_task.py
+++ api/message/celery_task.py
@@ -11,6 +11,6 @@ def send_sms_task(contacts: "list"):
 
 
 def send_mail_task(contacts: "list"):
-    send_mail(_("Validation"), _("message"), "test_no_reply@gmail.com", contacts)
+    send_mail(_("Validation"), _("message"), ["test_no_reply@gmail.com"], contacts)
     # print(f"Success MAIL WITH {settings.EMAIL_BACKEND} to {contacts}")
     return f"Success MAIL WITH {settings.EMAIL_BACKEND} to {contacts}"
